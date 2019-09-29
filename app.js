c48f76634c9a0e75682c409a9c5d4dcfab269829
Brandon Ellis <brandonellis@Brandons-MacBook-Pro.local>
2019-09-28 19:06:38 -0600

    fixed bugs

--- app.js
+++ app.js
@@ -2,21 +2,34 @@ $(function calc () {
 
     // used to store the nums & operators in the equation
     let arr = [];
+    let reference = false; // used to test if = sign was pushed
+    let referenceArr = []; // if = was used, tests if next item is an operator or num
 
     // adds the nums to the array (0-9)
     $('.calc-num').on('click', function() {
     
-        $('#current-num').html($(this).attr("value"));
+        $('#display').html($(this).attr("value"));
         let num = $(this).attr("value");
         arr.push(num);
-        console.log(arr);
 
+        // after an =, if a num is used, clears the array
+        if (reference) {
+            referenceArr.push(num);
+            if (referenceArr[1] === "0" || "1" || "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9") {
+                arr = [num];
+                referenceArr = [];
+            }
+            reference = false;
+        }
+        
+        console.log(arr);
     });
 
-    // adds decimal point
-    $('#decimal-point').on('click', function() {
+
+    //**** adds decimal point ****//
+    $('#decimal').on('click', function() {
     
-        //$('#current-num').html($(this).attr("value"));
+        //$('#display').html($(this).attr("value"));
         let decimalPoint = $(this).attr("value");
         arr.push(decimalPoint);
 
@@ -28,19 +41,33 @@ $(function calc () {
         }
 
         // looks for duplicate decimals in arr, doesn't allow second decimal
-        function checkIfArrayIsUnique(arr) {
+        function checkForDecimal(arr) {
             return arr.length === new Set(arr).size;
         }
 
-        checkIfArrayIsUnique(arr)
+        checkForDecimal(arr)
 
-        if (checkIfArrayIsUnique(arr) === false) {
+        if (checkForDecimal(arr) === false) {
             arr.pop()
         }
 
+        console.log(arr);
+
+        if (reference) {
+            referenceArr.push(decimalPoint);
+            if (referenceArr[1] === ".") {
+                arr = [decimalPoint];
+                reference = false;
+                referenceArr = []
+            }
+        }
+        console.log(arr);
+
     });
 
-    // adds the operator buttons to the array (+, -, /, *)
+
+
+    //**** */ adds the operator buttons to the array (+, -, /, *) ****//
     $('.calc-operator').on('click', function() {
     
         let operator = $(this).attr("value");
@@ -49,36 +76,47 @@ $(function calc () {
         // handles multiple operators used in a row
         if (arr[arr.length-2] === ("+" || "-" || "/" || "*")) {
             if (arr[arr.length-1] === ("+" || "-" || "/" || "*")) {
+                // arr.slice(arr.length-2,arr.length-2);
                 arr.pop();
             } 
         }
 
+        if (reference) {
+            referenceArr.push(operator);
+            reference = false;
+        }
+
+        //****need to remove operators except for the last one used; pop will not work
         console.log(arr)
 
 
     });
 
-    // joins & evaluates array; clears arr and adds answer; displays answer in answer div
+    // **** joins & evaluates array; clears arr and adds answer; displays answer in answer div ***//
     $('#equals').on('click', function() {
 
 
         $('#answer').html(Math.round(1000*eval(arr.join('')))/1000);
 
-        //IMMEDIATE EXECUTION LOGIC
-        //****** need to handle second operator used; when to clear arr or use next opperator
-        //if ()
-         arr = [eval(arr.join(''))]
+         arr = [eval(arr.join(''))];
+         reference = true;
+         referenceArr = [eval(arr.join(''))];
 
-         
+         console.log(arr);
+
+         //if num after click, reset arr and add it to the arr
 
     });
 
-    // clears the array with the "C" button; clears the answer div
+    
+    
+    // *** clears the array with the "C" button; clears the answer div ***//
     $('#clear').on('click', function() {   
         arr = [];
+        referenceArr = [];
+        reference = false;
+
         console.log("clear");
         $('#answer').html(arr);
     });
-
-
 })
