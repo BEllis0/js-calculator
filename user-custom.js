757207a6145f2068432326a57f0e9e30b53b299f
Brandon Ellis <brandonellis@Brandons-MacBook-Pro.local>
2019-09-29 21:03:23 -0600

    jquery to handle user custom colors

---
+++ user-custom.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+    function showDropdown() {
+        $('#custom-colors').toggleClass('show');
+    }
+    
+    $('#button').click(showDropdown);
+
+    // $(function() {
+        
+        
+
+
+    // })
+
+    
+})
+
+// function showDropdown() {
+//     $('#custom-colors').toggleClass('show');
+//     console.log('yes')
+// }
+
+// $('#button').click(showDropdown());
